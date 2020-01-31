aca3192e5bec5fe82d0c95b1f605ce64df96810a
Drew Waszak <35929280+DrewWazz@users.noreply.github.com>
2020-01-31 15:47:06 -0600

    Final Checkpoint 1 Commit

--- bundle.js
+++ bundle.js
@@ -1,11 +1,18 @@
 'use strict';
 
-// src/foo.js
-var foo = 'hello world!';
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-// src/main.js
-function main () {
-  console.log(foo);
+var format = _interopDefault(require('date-fns/format'));
+
+var span = document.querySelector('#time-now');
+
+function update() {
+	span.textContent = format(new Date(), 'h:mm:ssa');
+	setTimeout(update, 1000);
 }
 
-module.exports = main;
+// even though Rollup is bundling all your files together, errors and
+// logs will still point to your original source modules
+console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+
+update();
