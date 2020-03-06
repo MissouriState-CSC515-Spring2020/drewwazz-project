602d1c1bb904674e5818870148c46487a18cb627
Drew Waszak <35929280+DrewWazz@users.noreply.github.com>
2020-03-05 20:39:32 -0600

    added components

---
+++ src/components/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Whoops 404!! The page is not available.
+        <a href="#" @click.prevent="back">Go back</a>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        back() {
+            this.$router.go(-1);
+        }
+    }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+	<!--
+	<div>
+		<h1>Gibson Page</h1>
+		<p>The time is <span id='time-now'>...</span></p>
+	</div>
+	
+	<div id ="MagicCarousel" class ="carousel slide" data-ride="carousel">
+		<div class="carousel-inner" role="listbox">
+			<div class="carousel-item active">
+					<img src="./images/Gibson1.jpg" class = "d-block w-100" alt="Fourth slide">
+			</div>
+			<div class = "carousel-item">
+					<img src="./images/Gibson2.jpg" class = "d-block w-100" alt="Fifth slide">
+			</div>
+			<div class = "carousel-item">
+					<img src="./images/Gibson3.jpg" class = "d-block w-100" alt="Sixth slide">
+			</div>
+		</div>
+	</div>
+-->
+    <div>
+        Login
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
