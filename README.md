3ff5c4707a7157c3c382f7ae0b484f254aa2cb75
Drew Waszak <35929280+DrewWazz@users.noreply.github.com>
2020-05-05 10:13:43 -0500

    refactoring checkpoint 1

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ npm install
 
 ### Compiles for development
 ```
-npm run Start
+npm start
 ```
 
--- src/update.js
+++ src/update.js
@@ -1,8 +1,5 @@
-import format from 'date-fns/format';
+var span = document.querySelector('#HelloWorld');
 
-var span = document.querySelector('#time-now');
-
-export default function update() {
-	span.textContent = format(new Date(), 'h:mm:ssa');
-	setTimeout(update, 1000);
+export default function start() {
+	span.textContent = 'Hello World!';
 }
